218a6b4e8767c9d9d8565e38d3ed16375a288f10
smithge5 <gmansmith2020@gmail.com>
2019-10-03 14:51:53 -0400

    added iteration through jpg data

--- process.py
+++ process.py
@@ -1,27 +1,35 @@
 import PIL
-from PIL import Image
+import numpy as np
+from PIL import Image 
 #print image funciton
+
+pixel = np.array([],[])
 def show():
 	image.show()
 
 #grab information such as width, height, etc.
-def grabInfo():
+def imageProcess():
 	width, height = image.size
 	print("Width:", width, "\nHeight:", height)
+	#load the jpg information into a 2d numpy array
+	im_data = np.array(image)
+	print(im_data)
 
-
-
-
+	#iterate through pixel data
+	for x in range(width):
+		for y in range(height):
+			pixel = im_data[x][y]
+			print(pixel)
+	return 
 
 
 
 if __name__ == '__main__':
-
 	#read in the image and display the image. 
 	try:
 		image = Image.open('images\img1.jpg')
 		print('Successfully loaded image!')
-		grabInfo()
+		imageProcess()
 	except IOError:
 		print('An error occured trying to open the file.')
 
